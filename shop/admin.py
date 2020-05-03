02d0f6d0da540410625d8c41c5061718d8db4cff
Evert <evert.rot@gmx.com>
2020-05-03 23:46:15 +0200

    Created shipping object, foreignkeys SET_NULL

--- shop/admin.py
+++ shop/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import shop_items, work_types, materials, work_sizes
+from .models import shop_items, work_types, materials, work_sizes, shipping, reviews
 
 # Register your models here.
 admin.site.register(shop_items)
 admin.site.register(work_types)
 admin.site.register(materials)
 admin.site.register(work_sizes)
+admin.site.register(shipping)
+admin.site.register(reviews)
--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+from checkout.models import orders
 
 # Create your models here.
 
@@ -36,6 +38,30 @@ class work_sizes(models.Model):
         return self.name
 
 
+class shipping(models.Model):
+    """
+    Shipping cost per region
+    """
+    region = models.CharField(max_length=200)
+    price = models.DecimalField(
+        max_digits=5, decimal_places=2, default=0)
+
+    def __str__(self):
+        return self.region
+
+
+class reviews(models.Model):
+    """
+    Reviews for ordered items
+    """
+    free_text = models.CharField(max_length=3000, blank=True)
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    order = models.OneToOneField(orders, null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user
+
+
 class shop_items(models.Model):
     """
     Adding a work to the database
@@ -58,10 +84,12 @@ class shop_items(models.Model):
     signed = models.BooleanField(default=True)
     date_modified = models.DateField(auto_now=True)
     work_type = models.ForeignKey(
-        work_types, on_delete=models.CASCADE)
+        work_types, on_delete=models.SET_NULL, null=True)
     material = models.ForeignKey(
-        materials, on_delete=models.CASCADE)
+        materials, on_delete=models.SET_NULL, null=True)
     work_size = models.ForeignKey(
-        work_sizes, on_delete=models.CASCADE)
+        work_sizes, on_delete=models.SET_NULL, null=True)
     personal_message = models.CharField(max_length=200, blank=True)
+    personal_text = models.BooleanField(default=False)
+    standard_text = models.BooleanField(default=True)
     sort_order = models.SmallIntegerField(default=0)
