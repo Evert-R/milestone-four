f604de9ef42a984c62554c81529e62a493ac1939
Evert <evert.rot@gmx.com>
2020-05-07 22:54:32 +0200

    Fixed sort order in bootstrap columns from vert to horizontal

--- works/views.py
+++ works/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from works.models import work_items, work_images, categories
 from works.forms import FilterForm
+import math
 # Create your views here.
 
 
@@ -15,8 +16,32 @@ def all_works(request):
                 category=filter_results)
             return render(request, "works.html", {"works": works, "filter_form": filter_form})
     else:
-        works = work_items.objects.all()
-        return render(request, "works.html", {"works": works, "filter_form": filter_form})
+        works = work_items.objects.all().order_by('sort_order', 'id')
+        """
+        Because bootstrap orders card columns from top to bottom
+        we create a new list that orders them horizontally
+        """
+        view_works = []
+        # Get the total works count
+        total = works.count()
+        # Calculate how many rows we would get
+        rows = int(math.ceil(total / 3))
+        # Order the horizontally per row
+        for iterator in range(rows):
+            try:
+                view_works.append(works[iterator])
+            except:
+                pass
+            try:
+                view_works.append(works[iterator+rows])
+            except:
+                pass
+            try:
+                view_works.append(works[iterator+(rows*2)])
+            except:
+                pass
+
+        return render(request, "works.html", {"works": view_works, "filter_form": filter_form})
 
 
 def work_details(request, pk):
