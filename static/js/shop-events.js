640543973065bdcd8ed6943fb29986291ad65a71
Evert <evert.rot@gmx.com>
2020-05-11 12:02:17 +0200

    Shop items now link to items detail page

--- static/js/shop-events.js
+++
@@ -1,7 +0,0 @@
-$('#er-slide').slideUp();
-$('#er-hover-slide').hover(function () {
-    $('#er-slide').slideDown(500)
-});
-$('#er-hover-slide').mouseleave(function () {
-    $('#er-slide').slideUp(500)
-});
