58c8311c4b4022e94745a41438f7775d30cd9686
Evert <evert.rot@gmx.com>
2020-05-13 22:30:35 +0200

    Added footer with javascript events

--- static/js/base-events.js
+++ static/js/base-events.js
@@ -1,9 +1,27 @@
+// Make clicked nav button active
+
 window.onload = function () {
     $('.nav-item').on('click', function () {
         $('.nav-item').removeClass('active');
         $(this).addClass('active');
     })
-    setTimeout(function () {
-        $('.er-messages').slideUp(1000);
-    }, 2000);
+
 }
+
+/* 
+When scrolled up, the footer is sticky
+and only shows the artist links when viewed on mobile
+because otherwise it will fill up to much screen
+When we scroll down the footer jumps to the bottom of the page
+so it's not too intrusive
+*/
+
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 0) {
+        $('.er-footer').css('position', 'unset');
+        $('.er-web').removeClass('er-web-display')
+    } else {
+        $('.er-footer').css('position', 'fixed');
+        $('.er-web').addClass('er-web-display')
+    }
+});
