29acfd518e006cb20abb5e7b17f46e3b7a2f2448
Evert <evert.rot@gmx.com>
2020-05-13 22:31:37 +0200

    removed dashboard javascript - is handled with css now

--- static/js/dashboard-events.js
+++
@@ -1,7 +0,0 @@
-$('.er-nav-links').css('padding-top', '0');
-$('.navbar').css('transition', 'height 0s');
-$('body').css('padding-top', '3rem');
-$('.navbar').css('height', '3rem');
-$('.er-logo').slideUp();
-$('.navbar').css('min-height', 'unset');
-$('.er-nav-links').css('transition', 'padding-top 0s');
