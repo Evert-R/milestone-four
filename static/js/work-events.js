b75fed67ccb68c75607670c826f287db6d249bde
Evert <evert.rot@gmx.com>
2020-05-13 22:34:37 +0200

    Moved work javascript to seperate file

---
+++ static/js/work-events.js
@@ -0,0 +1,10 @@
+// Set menu item active
+$('#er-works-active').addClass('active');
+
+// Remove the shop navigation
+$('#er-shop-nav').addClass('d-none');
+
+// Make filter submit on select
+$('#id_cat').change(function () {
+    $('#er-filter-works').submit();
+})
