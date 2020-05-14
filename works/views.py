28321995f83c052d6287e5846a2553ae9f54573b
Evert <evert.rot@gmx.com>
2020-05-14 11:50:38 +0200

    Removed sorting for bootstrap columns as this is unpredictable

--- works/views.py
+++ works/views.py
@@ -17,38 +17,21 @@ def all_works(request):
             return render(request, "works.html", {"works": works, "filter_form": filter_form})
     else:
         works = work_items.objects.all().order_by('sort_order', 'id')
-        """
-        Because bootstrap orders card columns from top to bottom
-        we create a new list that orders them horizontally
-        """
-        view_works = []
-        # Get the total works count
-        total = works.count()
-        # Calculate how many rows we would get
-        rows = int(math.ceil(total / 3))
-        # Order the horizontally per row
-        for iterator in range(rows):
-            try:
-                view_works.append(works[iterator])
-            except:
-                pass
-            try:
-                view_works.append(works[iterator+rows])
-            except:
-                pass
-            try:
-                view_works.append(works[iterator+(rows*2)])
-            except:
-                pass
-
-        return render(request, "works.html", {"works": view_works, "filter_form": filter_form})
+        return render(request, "works.html", {"works": works, "filter_form": filter_form})
 
 
 def work_details(request, pk):
+    """ 
+    Display a works details 
+    and an overview of all works beneath
+    """
     work = work_items.objects.get(pk=pk)
     images = work_images.objects.filter(
         work_item_id=pk).order_by('sort_order', 'id')
-    return render(request, "workdetails.html", {"work": work, "images": images})
+    works = work_items.objects.all().order_by('sort_order', 'id')
+    return render(request, "workdetails.html", {"work": work,
+                                                "images": images,
+                                                "works": works, })
 
 
 def all_test(request):
