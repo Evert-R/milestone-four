2e6ccb6bc4467ffb2267f6e4903e1dd5032f3260
Evert <evert.rot@gmx.com>
2020-06-14 02:04:41 +0200

    Settings panels 2 columns - open panels on load events

---
+++ static/js/settings-events.js
@@ -0,0 +1,5 @@
+// Open panels, as there are only two at the moment
+// If in a future update more panels are created
+// This won't be necessary anymore
+$('#er-manage-shipping').collapse('show');
+$('#er-shop-message').collapse('show');
